cba2fa3248a34cbe853bf25ed9561a263041be8e
Rudra-CSER <djshina1900@gmail.com>
2025-12-23 03:28:01 +0530

    js Added

---
+++ JS/one.js
@@ -0,0 +1,26 @@
+//console log();
+//console info();
+//console warn();
+//console error();
+//cosole table();
+
+
+
+//msg.replace();   ---=- can be used in js inpect menu
+
+
+//operators 
+// ODD or EVEN
+// for(let t = 1 ; t<11 ; t++ ){
+//   if(t%2 == 0)console.log(`${t} -EVEN`);
+//   else console.log(`${t} -ODD`);
+
+// }
+
+// 
+
+const num =  prompt(`give some number `)
+if( num >=0 ){
+  console.log(`postive`);
+}else(console.log(`negative`)
+)
